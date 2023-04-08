5a903b01656b9cf4137eca7f7b1d4cbe550390b5
Your NameLiaaaaaaaaaaagit config --global user.email you@example.comyllia_vladimirovna@mail.ru <you@example.com>
2023-04-08 14:05:50 +0300

    feat: add active burgermenu

--- shelter/main.js
+++ shelter/main.js
@@ -1,2 +1,45 @@
-console.log( '100 баллов');
- 
+console.log('100 баллов');
+
+//----burgermenu------------
+const burgermenu = document.querySelector('.burgermenu');
+const headerNav = document.querySelector('nav');
+const burgerLine = document.querySelector('.burger__line');
+const nav = document.querySelector('nav');
+const navItem = nav.children;
+const html = document.querySelector('html');
+const headerContainer = document.querySelector('.header__container');
+
+console.log(navItem);
+
+// console.log(burgermenu)
+
+
+function burgermenuToggle() {
+    headerNav.classList.toggle('burgermenu__active');
+    burgermenu.classList.toggle('burgermenu-icon__active');
+    headerContainer.classList.toggle('header__container_active');
+    document.body.classList.toggle('lock');
+    // nav.classList.toggle('burgermenu__container');
+
+
+}
+
+burgermenu.addEventListener('click', burgermenuToggle);
+
+// function ClickNavLink(e) {
+//     console.log(nav)
+//         (navItem.map(e=> e.contains(e.target)) ? burgermenuOpen(): console.log('no') )
+// }
+
+// headerNav.addEventListener('click', ClickNavLink)
+
+function notClickBurgerIcon(e) {
+    if ((!burgermenu.contains(e.target) && headerNav.classList.contains('burgermenu__active') && !headerNav.contains(e.target))) {
+        burgermenuToggle();
+    } else if (nav.contains(e.target)) {
+        burgermenuToggle();
+    }
+}
+
+
+document.addEventListener('click', notClickBurgerIcon);
